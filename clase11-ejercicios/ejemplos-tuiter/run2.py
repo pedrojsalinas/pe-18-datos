f0f0b3e6362f29e2c66062a57f69def212333d37
reroes <reroes799@gmail.com>
2018-09-03 19:00:43 -0500

    clase 12 - couchdb

--- clase11-ejercicios/ejemplos-tuiter/run2.py
+++ clase11-ejercicios/ejemplos-tuiter/run2.py
@@ -1,5 +1,6 @@
 from credenciales import *
 import tweepy
+import codecs
 # https://tweepy.readthedocs.io/en/v3.5.0/
 
 # Access and authorize our Twitter credentials from credentials.py
@@ -8,7 +9,31 @@ auth.set_access_token(access_token, access_token_secret)
 api = tweepy.API(auth, parser=tweepy.parsers.JSONParser())
 
 # For loop to iterate over tweets with #ocean, limit to 10
-lista = ["usuarios-tuiter"]
+
+'followers_count', 
+'listed_count', 
+'friends_count', 
+'screen_name', 
+'favourites_count',
+'name',
+'created_at'
+
+lista = ["CarlosC62169047", "djosuejj", "pedrojsalinas", "fernandogdo1"]
 for l in lista:
-    user_data = api.get_user(l) 
-    print "%s - %s" % (l, user_data['created_at'])
+    archivo = codecs.open("%s.csv" % l, "w", encoding="utf8")
+    informacion = api.followers(l)
+    for u in informacion['users']:
+        data = {'followers_count': u['followers_count'], 
+                'listed_count': u['listed_count'], 
+                'friends_count':u['friends_count'], 
+                'screen_name':u['screen_name'], 
+                'favourites_count':u['favourites_count'],
+                'name':u['name'],
+                'created_at':u['created_at']
+                }
+        archivo.write(u"{screen_name}|{name}|{created_at}|{followers_count}|{listed_count}|\
+                {friends_count}|{favourites_count}\n".format(**data))
+    archivo.close()
+
+
+
